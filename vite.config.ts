4e0215cd934efc21523680985126da98bccbf6d3
娄松 <>
2024-12-10 13:34:44 +0800

    feat: cubic

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cubic',  // 默认，表示部署在根路径
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
